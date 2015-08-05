c3a392cf9165c0ed10d8c0053a44803b738e9e10
mieko nagatome <mieko_193@hotmail.com>
2015-08-04 20:02:26 -0500

    Swype Introduction

--- app/src/main/java/com/team16/antenna/trend/drivingassistancesystem/Introduction.java
+++ app/src/main/java/com/team16/antenna/trend/drivingassistancesystem/Introduction.java
@@ -1,38 +1,74 @@
 package com.team16.antenna.trend.drivingassistancesystem;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 
 public class Introduction extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
         setContentView(R.layout.activity_introduction);
+
+        PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager());
+        ViewPager mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(adapter);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_introduction, menu);
-        return true;
+    public void onClickButtonOBDConnection(View view){
+        Intent i=new Intent(Introduction.this,OBD.class);
+        startActivity(i);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public class PagerAdapter extends FragmentPagerAdapter {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        public PagerAdapter(FragmentManager fm) {
+            super(fm);
         }
 
-        return super.onOptionsItemSelected(item);
+        public Fragment getItem(int arg0) {
+            switch (arg0) {
+                case 0:
+                    return new Intro1();
+                case 1:
+                    return new Intro2();
+                case 2:
+                    return new Intro3();
+                case 3:
+                    return new Intro4();
+                case 4:
+                    onDestroy();
+                default:
+                    return null;
+            }
+        }
+
+        public int getCount() {
+            return 4;
+        }
+
+        public CharSequence getPageTitle(int position) {
+            String title = null;
+            switch (position) {
+                case 0:
+                    title = "1";
+                    break;
+                case 1:
+                    title = "2";
+                    break;
+                case 2:
+                    title = "3";
+                    break;
+                case 3:
+                    title = "4";
+                    break;
+            }
+            return title;
+        }
     }
 }
